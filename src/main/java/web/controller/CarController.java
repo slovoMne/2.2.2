8a3ff5b6a697ea4e213449cc8e659778abaa112b
Olga Yu <teta12@yandex.ru>
2021-12-12 15:32:30 +0500

    add controller

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,19 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 import web.service.CarServise;
+
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
     @Autowired
-     CarServise carServise;
+    private CarServise carServise;
 
     @GetMapping("")
-    public String getCarsByTotal(@RequestParam(value = "count", required = false) Integer count, Model model) {
-
-        model.addAttribute("garage", carServise.getCarsByTotal(count));
+    public String getCarsByTotal(@RequestParam(defaultValue = "5", value = "count",
+            required = false) Integer count, Model model) {
+        List<Car> cars = carServise.getCarsByTotal(count));
+        model.addAttribute("garage", cars);
         return "cars";
     }
 }
